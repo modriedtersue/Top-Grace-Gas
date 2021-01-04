9b19c925ecd1967f342e19b032028069cb65abd2
modriedtersue <eliakimmodried@gmail.com>
2021-01-04 02:26:03 -0800

    first commit

--- dashboard/pages/admin/collections.php
+++ dashboard/pages/admin/collections.php
@@ -4,14 +4,14 @@
         <div class="page-bar">
             <div class="page-title-breadcrumb">
                 <div class=" pull-left">
-                    <div class="page-title"> Collections</div>
+                    <div class="page-title"> Sales Collection</div>
                 </div>
                 <ol class="breadcrumb page-breadcrumb pull-right">
                     <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href=".">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
                     </li>
-                    <li><a class="parent-item" href="?p=collections">Collections</a>&nbsp;<i class="fa fa-angle-right"></i>
+                    <li><a class="parent-item" href="?p=collections">Sales</a>&nbsp;<i class="fa fa-angle-right"></i>
                     </li>
-                    <li class="active">Collection List</li>
+                    <li class="active">Collection</li>
                 </ol>
             </div>
         </div>
@@ -24,18 +24,31 @@
                     <div class="card-body ">
                         <form method="post" action="?p=collections">
                             <div class="row">
-                                <div class="col-lg-4 p-t-20">
+                            	 <div class="col-lg-3 p-t-20">
+									<select name="admin_id" id="admin_id" class="form-control">
+										<option value=""> -- select Admin --</option>
+										<?php
+											$run1 = $this->run("SELECT * FROM `admin`");
+											while ($row1 = $this->fetch($run1)) {
+												?>
+											<option value="<?php echo $row1['admin_id'] ?>"><?php echo $row1['username']; ?></option>
+												<?php
+											}
+										?>
+									</select>
+                                </div>
+                                <div class="col-lg-3 p-t-20">
                                     <div class="form-control-wrapper">
                                         <input type="text" id="date" name="date" class="floating-label mdl-textfield__input" placeholder="Date">
                                     </div>
                                 </div>
-                                <div class="col-lg-4 p-t-20">
+                                <div class="col-lg-3 p-t-20">
                                     <div class="form-control-wrapper">
                                         <input type="text" id="date1" name="date1" class="floating-label mdl-textfield__input" placeholder="Date">
                                     </div>
                                 </div>
-                                <div class="col-lg-4 p-t-20">
-                                    <button type="submit" class="btn btn-primary"> Submit</button>
+                                <div class="col-lg-3 p-t-20">
+                                    <button type="submit" name="btn_sort" class="btn btn-primary"> Submit</button>
                                 </div>
                             </div>
                         </form>
@@ -47,7 +60,7 @@
             <div class="col-sm-12">
                 <div class="card card-topline-red">
                     <div class="card-head">
-                        <header>Collection Table s</header>
+                        <header>Sales Collection</header>
                         <div class="tools">
                             <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
                             <a class="t-collapse btn-color fa fa-chevron-down"
@@ -60,25 +73,39 @@
                             <table class="table table-striped table-hover">
                                 <thead>
                                 <tr>
-                                    <th> # </th>
-                                    <th> Username </th>
-                                    <th> balance </th>
-                                    <th> Username <?php echo date('y-m-d') ?>  </th>
+									<th> Admin </th>
+									<th> From </th>
+									<th> To </th>
+									<th> Total KG </th>
+									<th> Collection </th>
                                 </tr>
                                 </thead>
                                 <tbody>
+                                
                                 <?php
-                                $run = $this->run("SELECT * FROM transactions WHERE date_sort BETWEEN '2020-11-01' AND '2020-11-05'");
+
+                                if (isset(($_POST['btn_sort']))){
+
+                                			$date1 = $this->clean($_POST['date1']);
+
+                                			$date = $this->clean($_POST['date']);
+                                			$admin_id = $this->clean($_POST['admin_id']);
+
+                                		$run = $this->run("SELECT *, SUM(`total_amount`) AS total_amunt, SUM(`kg`) AS total_kg FROM transactions WHERE `admin_id`='$admin_id' AND date_sort BETWEEN '$date' AND '$date1'");
                                 while($row = $this->fetch($run)){
                                     ?>
                                     <tr>
-                                        <td> 1 </td>
-                                        <td> <?php echo date('Y') ?> </td>
-                                        <td> Otto </td>
-                                        <td> makr124 </td>
+                                    	  <td> <?php echo 'modried' ?></td>
+                                        <td> <?php echo $date ?></td>
+                                        <td> <?php echo $date1 ?> </td>
+                                          <td> <?php echo $row['total_kg'] ?> </td>
+                                        <td> <b><?php echo $this->format_money($row['total_amunt'],true); ?></b> </td>
                                     </tr>
                                     <?php
+                                }	
+
                                 }
+                                
                                 ?>
                                 </tbody>
                             </table>
--- dashboard/pages/admin/view_user_transaction.php
+++ dashboard/pages/admin/view_user_transaction.php
@@ -17,12 +17,12 @@ if(empty($_GET['customer_serial_number'])){
         <div class="page-bar">
             <div class="page-title-breadcrumb">
                 <div class=" pull-left">
-                    <div class="page-title">Transactions</div>
+                    <div class="page-title">User Transactions</div>
                 </div>
                 <ol class="breadcrumb page-breadcrumb pull-right">
                     <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href=".">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
                     </li>
-                    <li><a class="parent-item" href="?p=transactions">Transactions</a>&nbsp;<i class="fa fa-angle-right"></i>
+                    <li><a class="parent-item" href="?p=transactions">User Transactions</a>&nbsp;<i class="fa fa-angle-right"></i>
                     </li>
                     <li class="active">Transactions Lists</li>
                 </ol>
